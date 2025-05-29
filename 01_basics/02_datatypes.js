5ce7e2569e7244f8c6a729f925dd85143edd3ec5
BhargavTikone22 <bhargavtikone2265@gmail.com>
2025-05-29 13:52:58 +0000

    data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) //we are using nodejs, not browser. In browser it gives alert message on screen i.e. 6
+
+// console.log(3
+//    +
+//    3);   //this is the wrong way 
+
+console.log(3+3) //code readability should be high, make code beautiful 
+console.log("Bhargav")
+
+let name = "Bhargav"
+let age = 19
+let isLoggedIn = false
+
+//number--> 2 to power to 53
+//bigint
+//string-->""
+//boolean-->true/false
+//null-->standalone value (space is empty)
+//undefined--> value is not assigned
+//symbol-->unique (we see it more in react this channel)
+
+//object
+
+console.log(typeof "Bhargav");  //type of "bhargav" is string
+
+console.log(typeof null); //here type is object
+
+console.log(typeof undefined) //here it is undefined data type
