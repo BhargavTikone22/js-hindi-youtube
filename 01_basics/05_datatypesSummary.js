ddd3277609acc0cdc510bc05d48ae768e9565d77
BhargavTikone22 <bhargavtikone2265@gmail.com>
2025-06-02 11:35:23 +0000

    datatypes summary

---
+++ 01_basics/05_datatypesSummary.js
@@ -0,0 +1,8 @@
+//Primitive
+
+//7 types: String, Number, Boolean, Null, Undefined, BigInt, Symbol
+
+// Reference(Non-Primitve)
+// These are values in which memory reference is directly allocate.
+
+// Types: Array, Objects, Functions
