08b6497c246f9a275e76388bb0a158d7167d1b41
BhargavTikone22 <bhargavtikone2265@gmail.com>
2025-06-07 15:47:37 +0000

    data types summary

--- 01_basics/05_datatypesSummary.js
+++ 01_basics/05_datatypesSummary.js
@@ -2,7 +2,34 @@
 
 //7 types: String, Number, Boolean, Null, Undefined, BigInt, Symbol
 
+const score = 100 //string
+const scoreValue = 100.3 //number
+
+const loggedIn = false //boolean
+const outsideTemp = null //null- empty space
+let userEmail; //undefined
+
+const id = Symbol('123') //symbol
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+const bigNumber = 378988913879502849n //bigint
+
 // Reference(Non-Primitve)
 // These are values in which memory reference is directly allocate.
 
 // Types: Array, Objects, Functions
+
+const heros = ["Shaktiman", "Naagraj", "Doga"] //array
+//object
+let myObj = {
+    name: bhargav, //everything in curly braces {} is object
+    age:20,
+}
+
+//function
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof score);
